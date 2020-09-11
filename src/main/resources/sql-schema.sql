da232e206cb2998b26cff48cc869e5e19d85e35e
Mohammad Rashid <MRashid1@qa.com>
2020-09-11 15:23:35 +0100

    Added sql statements to create Items table in schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -2,8 +2,16 @@ drop schema ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`customer_id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`item_name` VARCHAR(40) NULL DEFAULT NULL,
+	`price` DECIMAL(19,4) NULL DEFAULT NULL,
+	`quantity` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`item_id`)
+); 
