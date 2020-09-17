2df1635e373e4b5eb5590554ebf4c244d59ffd8f
Mohammad Rashid <MRashid1@qa.com>
2020-09-17 09:46:30 +0100

    Created test class for ItemController

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -58,7 +58,7 @@ public class ItemDAO implements Dao<Item> {
 			LOGGER.debug(e);
 			LOGGER.error(e.getMessage());
 		}
-		return null;
+		return readLatest();
 	}
 
 	public Item readItem(Long id) {
--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,2 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`item_name`, `price`) values ('Phone', 499.99);
